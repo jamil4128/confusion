1180e8014640adc20f9ac660ea1b88d89c21d20a
Jamil <jamilahmedchowdhury4128@gmail.com>
2020-07-05 05:45:45 +0600

    end of assignment 4

---
+++ css/stylish.scss.d.ts
@@ -0,0 +1,13 @@
+import { StyleProp, TextStyle } from 'react-native'
+
+export const row-header: StyleProp<TextStyle>
+export const row-content: StyleProp<TextStyle>
+export const footer: StyleProp<TextStyle>
+export const jumbotron: StyleProp<TextStyle>
+export const navbar-dark: StyleProp<TextStyle>
+export const tab-content: StyleProp<TextStyle>
+export const carousel: StyleProp<TextStyle>
+export const carousel-item: StyleProp<TextStyle>
+export const modal-body: StyleProp<TextStyle>
+export const modal-header: StyleProp<TextStyle>
+export const close: StyleProp<TextStyle>
